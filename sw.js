aa44823b978ada8a16da69364270243d4d301eca
Alpha Picker <157329784+AlphaPicker@users.noreply.github.com>
2026-09-17 13:52:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. Définissez la nouvelle version du cache
-const CACHE_NAME = 'klon-stake-v22'; // <-- À incrémenter à chaque mise à jour (v2, v3, etc.)
+const CACHE_NAME = 'klon-stake-v23'; // <-- À incrémenter à chaque mise à jour (v2, v3, etc.)
 
 const ASSETS = [
   './',
